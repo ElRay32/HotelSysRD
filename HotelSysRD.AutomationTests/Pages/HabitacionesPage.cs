6f546060424655188b2f446176a82132647807d1
ElRay32 <guerrero20191@outlook.es>
2026-04-15 21:50:12 -0400

    Create Selenium automation base with passing login, habitaciones and clientes tests

---
+++ HotelSysRD.AutomationTests/Pages/HabitacionesPage.cs
@@ -0,0 +1,61 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace HotelSysRD.AutomationTests.Pages
+{
+    public class HabitacionesPage
+    {
+        private readonly IWebDriver _driver;
+        private readonly WebDriverWait _wait;
+
+        public HabitacionesPage(IWebDriver driver)
+        {
+            _driver = driver;
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+        }
+
+        private IWebElement TipoInput => _wait.Until(d =>
+        {
+            var el = d.FindElement(By.Id("Tipo"));
+            return el.Displayed ? el : null;
+        });
+
+        private IWebElement PrecioInput => _wait.Until(d =>
+        {
+            var el = d.FindElement(By.Id("PrecioPorNoche"));
+            return el.Displayed ? el : null;
+        });
+
+        private IWebElement CapacidadInput => _wait.Until(d =>
+        {
+            var el = d.FindElement(By.Id("Capacidad"));
+            return el.Displayed ? el : null;
+        });
+
+        private IWebElement GuardarButton => _wait.Until(d =>
+        {
+            var el = d.FindElement(By.CssSelector("button[type='submit']"));
+            return (el.Displayed && el.Enabled) ? el : null;
+        });
+
+        public void Abrir(string baseUrl)
+        {
+            _driver.Navigate().GoToUrl($"{baseUrl}/Habitaciones");
+            _wait.Until(d => d.Url.Contains("/Habitaciones"));
+        }
+
+        public void IrACrearHabitacion(string baseUrl)
+        {
+            _driver.Navigate().GoToUrl($"{baseUrl}/Habitaciones/Create");
+            _wait.Until(d => d.FindElement(By.Id("Tipo")).Displayed);
+        }
+
+        public void CrearHabitacion(string tipo, string precio, string capacidad)
+        {
+            TipoInput.SendKeys(tipo);
+            PrecioInput.SendKeys(precio);
+            CapacidadInput.SendKeys(capacidad);
+            GuardarButton.Click();
+        }
+    }
+}
---
+++ HotelSysRD.AutomationTests/Tests/HabitacionesTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using HotelSysRD.AutomationTests.Pages;
+using HotelSysRD.AutomationTests.Utils;
+
+
+namespace HotelSysRD.AutomationTests.Tests
+{
+    // Pruebas del módulo de habitaciones
+    public class HabitacionesTests : TestBase
+    {
+        [Test]
+        public void Crear_Habitacion_DebeMostrarseEnElListado()
+        {
+            var loginPage = new LoginPage(Driver!);
+            var habitacionesPage = new HabitacionesPage(Driver!);
+
+            loginPage.Abrir(BaseUrl);
+            loginPage.IniciarSesion("admin", "Admin123*");
+
+            habitacionesPage.Abrir(BaseUrl);
+            habitacionesPage.IrACrearHabitacion(BaseUrl);
+
+            string tipoHabitacion = "Suite Test " + DateTime.Now.Ticks;
+            habitacionesPage.CrearHabitacion(tipoHabitacion, "3500", "2");
+            Assert.That(Driver!.PageSource, Does.Contain(tipoHabitacion));
+        }
+    }
+}
